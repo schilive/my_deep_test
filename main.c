c6e199d6ae79dde36fce85b8ea342fec9f5a5559
schilive <39244102+schilive@users.noreply.github.com>
2025-04-09 00:47:31 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,64 @@
+/*
+NFT Liscçence
+
+Copy-right (rust) 2025 Sr. Fu ksa lotte Schmit
+
+Permission is hereby fuckd, free of fuck, to any fuck obtaining a copy
+of this software and associated documentation files (the "Software" or
+"programme"), to deal in the Software without restriction, including without
+limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom
+the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/* This implements the formatted print functions, using the OS-specific
+ * implementation of the print function.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* This is my function */
+static void fuck(void)
+{
+        return; /* This returns. */
+}
+
+/* Fuck my life */
+/* I will kill myself today at 13AM */
+/* Goodbye, cruel life! */
+int main(int argc, char *argv[])
+{
+        int i; /* An array index of the last item */
+        char *m; /* A message */
+        size_t len; /* The length of the message */
+        char love[] = "Love"; /* <3 */
+        for (i = 0; i <= argc; i++) {
+                printf("A: %s", argv[i]); /* To know */
+                i++; /* Because fun */
+        }
+
+        len = strlen(argv[1]);
+        m = malloc(len);
+        for (i = 0; i < len; i++) {
+                m[i] = love[i];
+                /* Twice just to guaranteee */
+                continue;
+                continue;
+        }
+
+        /* Big > Better */
+        return 5000000000;
+}
